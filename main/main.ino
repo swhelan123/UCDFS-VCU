dbe6b1b7ceab9f096cf2b02d3f0555cb707a03ba
Shane Whelan <shane.whelan2@ucdconnect.ie>
2024-10-26 01:32:07 +0100

    Added DEBUG_MODE and moved brake light stuff to its own func

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,7 @@
-#define BRAKE_LIGHT_PIN 9     // Pin 9 for LED (simulating brake light)
-#define BRAKE_PRESSURE_SENSOR_PIN A0  // Analog pin A0 for potentiometer (brake pressure sensor)
-#define THRESHOLD 500         // Threshold for when brake light should activate
+#define BRAKE_LIGHT_PIN 9                // Pin 9 for brake light
+#define BRAKE_PRESSURE_SENSOR_PIN A0     // Analog pin A0 for potentiometer (brake pressure sensor)
+#define THRESHOLD 500                    // Threshold for when brake light should activate (needs calibration)
+#define DEBUG_MODE 1                     // set debug mode to true in order to read serial outputs
 
 void setup() {
   // Initialize brake light pin as output
@@ -8,26 +9,34 @@ void setup() {
 
   // Initialize serial communication for debugging over USB
   Serial.begin(9600);
-
-  
 }
 
-int cycle = 0;
-
 void loop() {
-  // read analog input from brake pressure sensor
-  int brakePressure = analogRead(BRAKE_PRESSURE_SENSOR_PIN);
+  // function call to read brake pressure and handle brake light logic
+  brake_light();
 
-  // Print the brake pressure value to the serial monitor for debugging
-  Serial.print("Brake Pressure (A0): ");
-  Serial.println(brakePressure);
+  if (DEBUG_MODE);
+    delay(200);  // small delay to stabilize readings and not overwhelm the serial output, probs remove this in final code
+}
+
+void brake_light(){
+    // read analog input from brake pressure sensor
+  int brakePressure = analogRead(BRAKE_PRESSURE_SENSOR_PIN);
+  
+  if (DEBUG_MODE){
+    // Print the brake pressure value to the serial monitor for debugging
+    // Serial.print("Brake Pressure (A0): ");
+    Serial.println(brakePressure);
+  }
 
   // if brake pressure is above the threshold, turn on the brake light
   if (brakePressure > THRESHOLD) {
     digitalWrite(BRAKE_LIGHT_PIN, HIGH);
+    if (DEBUG_MODE)
+      Serial.println("Brake Light On\n");      // print brake light status to serial monitor 
   } else {
     digitalWrite(BRAKE_LIGHT_PIN, LOW);
+    if (DEBUG_MODE)
+      Serial.println("Brake Light Off\n");     // print brake light status to serial monitor
   }
-
-  delay(50);  // small delay to stabilize readings and not overwhelm the serial output, probs remove this in final code
 }
